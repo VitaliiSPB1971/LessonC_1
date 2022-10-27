a9f1231377edb5abbb7c3a8786887c7fad3712d1
Vitalii <telexp@yandex.ru>
2022-10-27 19:57:10 +0300

    add: Eample008 programm data

--- Example008/Program.cs
+++ Example008/Program.cs
@@ -1,20 +1,25 @@
-﻿int count = 0;
-int distance = 10000;
-int firstSpeed = 1;
-int secondSpeed = 2;
-int dogSpeed = 5;
-bool friend = true;
-int time = 0;
-while (distance > 10)
-{
-if (friend == false)
- { time = distance / (firstSpeed + dogSpeed);
-   friend = true;
-        time = distance / (secondSpeed + dogSpeed);
-        friend = false;
-distance = distance - (firstSpeed + secondSpeed) * time;
-count++;
- }
+﻿using System;
+class HelloWorld {
+  static void Main() {
+    int count = 0;
+    int distance = 10000;
+    int firstSpeed = 1;
+    int secondSpeed = 2;
+    int dogSpeed = 5;
+    int time = 0;
+    while (distance > 10)
+    {
+        if(count % 2 == 0) {
+            time = distance / (secondSpeed + dogSpeed);
+        } else {
+            time = distance / (firstSpeed + dogSpeed);
+        }
+        
+        distance = distance - (firstSpeed + secondSpeed) * time;
+        count++;
     }
-Console.Write("count = ");
-Console.WriteLine(count);
+    Console.Write("count = ");
+    Console.WriteLine(count);
+
+  }
+}
