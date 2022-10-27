f15088cc68805c8e4d9202953240165484a3431b
Vitalii <telexp@yandex.ru>
2022-10-27 19:32:42 +0300

    init: Example 008 Dog

---
+++ Example008/Program.cs
@@ -0,0 +1,20 @@
+﻿int count = 0;
+int distance = 10000;
+int firstSpeed = 1;
+int secondSpeed = 2;
+int dogSpeed = 5;
+bool friend = true;
+int time = 0;
+while (distance > 10)
+{
+if (friend == false)
+ { time = distance / (firstSpeed + dogSpeed);
+   friend = true;
+        time = distance / (secondSpeed + dogSpeed);
+        friend = false;
+distance = distance - (firstSpeed + secondSpeed) * time;
+count++;
+ }
+    }
+Console.Write("count = ");
+Console.WriteLine(count);
