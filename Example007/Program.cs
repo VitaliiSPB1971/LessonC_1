d2067b316c186b673dd7311c5cc8847d060699f7
Vitalii <telexp@yandex.ru>
2022-10-30 12:45:37 +0300

    init: Lesson 1 Hometask 3

---
+++ Example007/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите натуральное число ");
+int x=Convert.ToInt32 ( Console.ReadLine());
+int y=2;
+while (y <= x)
+{
+    if(y % 2 == 0 && y != x){
+        Console.Write(y + ", ");
+    }
+    if(y % 2 == 0 && y == x) {
+        Console.Write(y + " ");
+    }
+    y++;
+}
